2462d94150bc0ee22c4675f80df95af3370b3ea0
Vansh <66193724+VanshCodes@users.noreply.github.com>
2021-04-22 14:59:54 +0530

    initial commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,52 @@
 <script>
-	export let name;
+	import ConcactCard from "./ConcactCard.svelte";
+
+	export let name, age, data;
+	$: upperName = name.toUpperCase();
+	$: if (name == "vansh") {
+		name = "Aloo Khake";
+	}
+	$: data = {
+		title: "Hello",
+		content: "DATA",
+	};
+
+	$: console.log("null Ljnsdnkdsknkdns");
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello {upperName} !</h1>
+	<p>Age: {age}</p>
+	<button on:click={() => (age += 1)}> Add Age </button>
+	<!-- <button on:click="{() => name+=1}"></button> -->
+	<input type="text" bind:value={name} />
+<div class="card-items">
+	<ConcactCard {...data} />
+	<ConcactCard {...data} />
+	<ConcactCard {...data} />
+	<ConcactCard {...data} />
+	<ConcactCard {...data} />
+</div>
 </main>
 
 <style>
+	:root {
+		--text-primary: #eee4e7;
+		--text-secondary: yellow;
+		
+	}
+	input{
+		background-color:blueviolet
+	}
+	* {
+		color: var(--text-primary);
+		font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+	}
+	
+
+	p {
+		color: var(--text-secondary);
+	}
 	main {
 		text-align: center;
 		padding: 1em;
@@ -16,7 +55,7 @@
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: var(--text-primary);
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
@@ -26,5 +65,15 @@
 		main {
 			max-width: none;
 		}
+	}
+	button {
+		width: clamp(50px, 100px, 120px);
+		height: clamp(50px, 100px, 120px);
+		background-color: #a35709;
+	}
+	.card-items{
+		display:grid;
+        grid-template-columns: auto auto auto;
+
 	}
 </style>
---
+++ src/ConcatCard.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let title, content;
+</script>
+
+<div class="card">
+    <div class="title item">{title}</div>
+    <div class="content item">{content}</div>
+</div>
+
+<style>
+    .card {
+      display: flex;
+      width: clamp(100px, 150px, 200px);
+      height: clamp(100px, 150px, 200px);
+
+      background-color: black;
+      padding: 20px;
+      border-radius: 25%;
+      justify-content: center;
+      flex-direction: column;
+    }
+    .title {
+      font-family: fantasy;
+      font-size: 3rem;
+    }
+</style>
